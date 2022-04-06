0a1ee6d522e2e512e6c1e7f1151aac07c2d00ef6
Banerjee <chayan.banerjee@uon.edu.au>
2022-04-06 07:22:52 +0530

    initial commit

---
+++ build_my_world/script/hello.cpp
@@ -0,0 +1,18 @@
+// Print  message Welcome to Chayan's World
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("\n Welcome to Chayan's World \n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
